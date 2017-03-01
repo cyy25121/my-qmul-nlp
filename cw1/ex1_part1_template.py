7190e892195f02f09c60a2415be04bb03e82f5d9
YY <y.chang@se16.qmul.ac.uk>
2017-03-01 16:16:57 +0000

    cw1 v1

--- cw1/ex1_part1_template.py
+++ cw1/ex1_part1_template.py
@@ -2,78 +2,122 @@
 
 import unicodecsv			# csv reader
 from sklearn.svm import LinearSVC
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
 from nltk.classify import SklearnClassifier
 from random import shuffle
+from datetime import datetime
+from datetime import timedelta
 
 
 # DATA LOADING AND PARSING
 
 # convert line from input file into a datetime/string pair
 def parseTweet(tweetLine):
-	# should return a pair of a datetime object and a string containing the tweet message
-	return
+    dt = datetime.strptime(tweetLine[1], '%Y-%m-%d %H:%M:%S')
+    s = tweetLine[4]
+    # should return a pair of a datetime object and a string containing the tweet message
+    return (dt, s)
 
 # load data from a file and append it to the tweetData
 def loadData(path, label, tweet=None):
-	with open(path, 'rb') as f:
-		reader = unicodecsv.reader(f, encoding='utf-8')
-		reader.next()
-		for line in reader:
-			(dt,tweet) = parseTweet(line)
-			tweetData.append((dt,preProcess(tweet),label))
-			trainData.append((toFeatureVector(preProcess(tweet)),label))
+    with open(path, 'rb') as f:
+        reader = unicodecsv.reader(f, encoding='utf-8')
+        reader.next()
+        for line in reader:
+            (dt,tweet) = parseTweet(line)
+            tweetData.append((dt,preProcess(tweet),label))
+            trainData.append((toFeatureVector(preProcess(tweet)),label))
 
 # load application data
 def loadApplicationData(path):
-	with open(path, 'rb') as f:
-		reader = unicodecsv.reader(f, encoding='utf-8')
-		reader.next()
-		for line in reader:
-			londonTweetData.append(parseTweet(line))
+    with open(path, 'rb') as f:
+        reader = unicodecsv.reader(f, encoding='utf-8')
+        reader.next()
+        for line in reader:
+            londonTweetData.append(parseTweet(line))
 
 # TEXT PREPROCESSING AND FEATURE VECTORIZATION
 
 # input: a string of one tweet
 def preProcess(text):
-	# should return a list of tokens
-    return
+    # should return a list of tokens
+    s = text.split(' ')
+    return s
 
 # input: a tokenised sequence
 # you can optionally keep track of a global feature list that is a list of all the features (or words) that you encounter while going through the dataset
 # featureList = {} 
 def toFeatureVector(words):
     featureVector = {}
-	# return a dictionary 'featureVect' where the keys are the tokens in 'words' and the values are the number of occurrences of the tokens
+    for w in words:
+        if w in featureVector:
+            #featureVector[w] = featureVector[w] + 1
+            pass
+        else:
+            featureVector[w] = 1
+    # return a dictionary 'featureVect' where the keys are the tokens in 'words' and the values are the number of occurrences of the tokens
     return featureVector
+    # return {w: 1, 0 for w in words}
 
 # TRAINING AND VALIDATING OUR CLASSIFIER
 
 def trainClassifier(trainData):
-	print "Training Classifier..."
-	return SklearnClassifier(LinearSVC()).train(trainData)
+    print "Training Classifier..."
+    return SklearnClassifier(LinearSVC()).train(trainData)
 
 def crossValidate(dataset, folds):
-	shuffle(dataset)
-	results = []
-	foldSize = len(dataset)/folds
-	for i in range(0,len(dataset),foldSize):
-		pass
-		# insert code here that trains and tests on the 10 folds of data in the dataset
-	return results
+    shuffle(dataset)
+    results = []
+    foldSize = len(dataset)/folds
+    for i in range(0,len(dataset),foldSize):
+        clf = trainClassifier(dataset[:i] + dataset[i+foldSize:])
+        pLabel = predictLabels(dataset[i:i+foldSize], clf)
+        yLabel = [ l[1] for l in dataset[i:i+foldSize]]
+        results.append((accuracy_score(yLabel, pLabel), precision_recall_fscore_support(yLabel, pLabel)))
+        # insert code here that trains and tests on the 10 folds of data in the dataset
+    return results
 
 # PREDICTING LABELS GIVEN A CLASSIFIER
 
 def predictLabels(tweetData, classifier):
-	return classifier.classify_many(map(lambda t: toFeatureVector(preProcess(t[1])), tweetData))
+    test = map(lambda t: t[0], tweetData)
+    #return classifier.classify_many(map(lambda t: toFeatureVector(preProcess(t[1])), tweetData))
+    return classifier.classify_many(test)
 
 def predictLabel(text, classifier):
-	return classifier.classify(toFeatureVector(preProcess(text)))
+    return classifier.classify(toFeatureVector(preProcess(text)))
 
 # COMPUTING ANGER LEVEL ON A SET OF TWEETS
 
 def findAngerLevels(tweetData, classifier):
-	# should return a list of anger level for a period of time
-	return
+    # should return a list of anger level for a period of time
+    sorttweetData = sorted(tweetData)
+    hist = []
+    i = 0
+    for s in sorttweetData:
+        i = i + 1
+        pLabel = predictLabel(s[1], classifier)
+        hist.append((s[0], pLabel))
+
+    initD = hist[0][0]
+    delta = timedelta(1)
+    idxD = initD + delta
+    new_hist = []
+    count = {'angry':0, 'happy':0}
+    print(idxD)
+    for h in hist:
+        while h[0] >= idxD:
+            idxD = idxD + delta
+            new_hist.extend(count)
+            count['angry'] = 0
+            count['happy'] = 0
+        if h[1] == 'happy':
+            count['happy'] = count['happy'] + 1
+        else:
+            count['angry'] = count['angry'] + 1
+
+    return new_hist
 
 # MAIN
 
@@ -89,9 +133,10 @@ angryLabel = 'angry'
 happyLabel = 'happy'
 
 # references to the data files
-angryPath = 'angry_tweets.csv'
-happyPath = 'happy_tweets.csv'
-londonPath = 'london_2017_tweets.csv'
+datasetPath = 'dataset/tweets/'
+angryPath = datasetPath + 'angry_tweets.csv'
+happyPath = datasetPath + 'happy_tweets.csv'
+londonPath = datasetPath + 'london_2017_tweets.csv'
 
 
 ## In order to test the code in this
@@ -101,13 +146,13 @@ print "Loading happy tweets..."
 loadData(happyPath, happyLabel)
 print "Loading angry tweets..."
 loadData(angryPath, angryLabel)
-print 'number of words: ' + str(len(featureDict))
+#print 'number of words: ' + str(len(featureDict))
 cv_results = crossValidate(trainData, 10)
 print cv_results
-classifier = trainClassifier(trainData)
+#classifier = trainClassifier(trainData)
 print "Loading London data"
-loadApplicationData(londonPath)
+#loadApplicationData(londonPath)
 print "Computing anger levels!"
-angerLevels = findAngerLevels(londonTweetData, classifier)
-anger_peaks = None
-print anger_peaks
+#angerLevels = findAngerLevels(londonTweetData, classifier)
+#anger_peaks = None
+#print anger_peaks
--- cw1/ex1_part2_template.py
+++ cw1/ex1_part2_template.py
@@ -4,6 +4,13 @@
 from nltk.probability import ConditionalFreqDist
 from nltk.probability import ConditionalProbDist, LaplaceProbDist, MLEProbDist
 
+from datetime import datetime
+from datetime import timedelta
+
+from ex1_part1_template import loadApplicationData
+from ex1_part1_template import preProcess
+
+
 ##############
 ### PART B ###
 ##############
@@ -17,19 +24,52 @@ from nltk.probability import ConditionalProbDist, LaplaceProbDist, MLEProbDist
 # are observed in the list.
 
 def getBigrams(tweets):
-	return "TODO"
+    newT = []
+    for T in tweets:
+        first = True
+        for t in T:
+            if first is True:
+                pre = t
+                first = False
+            else:
+                cur = t
+                newT.append((pre, cur))
+                pre = cur
+    return newT
 
 # conditionalProbDist will return a probability distribution over a list of
 # bigrams, together with a specified probability distribution constructor
 def conditionalProbDist(probDist, bigrams):
-	cfDist = ConditionalFreqDist(bigrams)
-	cpDist = ConditionalProbDist(cfDist, probDist, bins=len(bigrams))
-	return cpDist
+    cfDist = ConditionalFreqDist(bigrams)
+    cpDist = ConditionalProbDist(cfDist, probDist, bins=len(bigrams))
+    return cpDist
 
 # this is the function where you can put your main script, which you can then
 # toggle if for test purposes
 def mainScript():
-	return "TODO"
+
+    londonTweetData = []
+    datasetPath = 'dataset/tweets/'
+    londonPath = datasetPath + 'london_2017_tweets.csv'
+
+    loadApplicationData(londonPath)
+    sorted(londonTweetData)
+
+    londonallData = []
+    london5thData = []
+    london9thData = []
+
+    for d in londonTweetData:
+        if d[0] >= datetime(2017, 1, 5) and d[0] < datetime(2017, 1, 6):
+            london5thData.append(preProcess(d[1]))
+        elif d[0] >= datetime(2017, 1, 9) and d[0] < datetime(2017, 1, 10):
+            london9thData.append(preProcess(d[1]))
+        londonallData.append(preProcess(d[1]))
+
+    s = getBigrams(london5thData)
+
+    return s
 
 # The line below can be toggled as a comment to toggle execution of the main script
-# results = mainScript()
+results = mainScript()
+print results
