b2d21aa49b76ad4387114fb18e054a8e777ace21
YY <y.chang@se16.qmul.ac.uk>
2017-03-23 21:07:57 +0000

    CW3 initial commit.

---
+++ cw3/semParser.py
@@ -0,0 +1,18 @@
+#/usr/bin/python
+
+import nltk
+
+#parser=nltk.load_parser('semGrammarFOLE.fcfg')
+parser=nltk.load_parser('semGrammarSimple.fcfg')
+
+def semParse(sentence):
+    print "Parsing \"" + sentence + "\":"
+    tree_generator = parser.parse(sentence.split())
+    for tree in tree_generator: 
+        print "  type:", tree.label()['SEM'].type
+        print "    LF:", tree.label()['SEM']
+        # for subtree in tree.subtrees():
+        #     print subtree.label()['SEM'].type, subtree.label()['SEM']
+
+semParse("show me the meals")
+semParse("show me the meals on the flights to Phoenix")
